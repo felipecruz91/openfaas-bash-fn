a14b06e187f50ba930ec619b6898d278677b6473
felipecruz91 <felipecruz91@hotmail.es>
2021-12-04 15:52:19 +0100

    Add authentication

--- bash-fn/provision.sh
+++ bash-fn/provision.sh
@@ -9,18 +9,33 @@ parse_params() {
       name="${2-}"
       shift
       ;;
+    -k | --key) # example named parameter
+      key="${2-}"
+      shift
+      ;;      
     *) break ;;
     esac
     shift
   done
 
   # check required params and arguments
-  [[ -z "${name-}" ]] && die "Missing required parameter: name"
+  [[ -z "${key-}" ]] && die "Missing required parameter: key";
+  [[ -z "${name-}" ]] && die "Missing required parameter: name";
 
   return 0
 }
 
 parse_params "$@"
+
+SECRET_API_KEY=$(cat /var/openfaas/secrets/secret-api-key)
+export SECRET_API_KEY
+
+if [ "$key" = "$SECRET_API_KEY" ]; then
+  echo "Keys are equal!"
+else
+  echo "Authentication failed"
+  exit 1
+fi
 
 TF_VAR_name=${name}
 export TF_VAR_name
